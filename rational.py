ae80414b2b17f86ae5d4b6765fa2e424c7f3aff0
FUJIWARA Yuka <21716081@jwu.ac.jp>
2019-10-07 16:25:59 +0900

    rational5

--- rational.py
+++ rational.py
@@ -14,8 +14,32 @@ class Q(object):
         c = q.a
         d = q.b
         return Q(a*d+b*c,b*d)
+
+    def sub(self,q):
+        a = self.a 
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d-b*c,b*d)
+
+    def mul(self,q):
+        a = self.a 
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*c,b*d)
+
+    def truediv(self,q):
+        a = self.a 
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(a*d,b*c)
         
 
 q1 = Q(1,2)
 q2 = Q(1,3)
 print(q1.add(q2))
+print(q1.sub(q2))
+print(q1.mul(q2))
+print(q1.truediv(q2))
